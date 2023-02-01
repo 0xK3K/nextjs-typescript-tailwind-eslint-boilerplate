7b2d356ca8afe261c8036d85a1bac0d75a9e2e11
0xK3K <0xk3k@protonmail.com>
2023-02-01 12:59:49 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/* eslint no-undef: 0 */
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/* eslint no-undef: 0 */
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
